87d99d35d71331feb991961ae6943e6fdff16657
Riccardo Amadio <riccardo.amadio1@gmail.com>
2021-04-08 18:30:29 +0200

    change name variable prot

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 80;
+const port  = process.env.PORT || 80;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

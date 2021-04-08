efea4b3a7a79e9ec0b613ed6abefcb6352a2df40
Riccardo Amadio <riccardo.amadio1@gmail.com>
2021-04-08 18:43:27 +0200

    mongodb add

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 const port  = process.env.PORT || 80;
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ciao:ciao@cluster0.zofui.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  client.connect(err => {
+    let dbo = client.db("myFirstDatabase").collection("devices");
+    dbo.createCollection("customers", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      res.send("collection created")
+      db.close();
+    });
+    // perform actions on the collection object
+    client.close();
+  });
+  
 })
 
 app.listen(port, () => {

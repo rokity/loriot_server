f3a2aae9e5e6f4cacc59b263a284821a9b583690
Riccardo Amadio <riccardo.amadio1@gmail.com>
2021-04-09 12:26:09 +0200

    webhook added

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express')
+var bodyParser = require('body-parser');
 const app = express()
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 const port  = process.env.PORT || 80;
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://ciao:ciao@cluster0.zofui.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log("ciao")
+let dbo = client.db("myFirstDatabase");
 
-app.get('/', (req, res) => {
+app.post('/webhook', (req, res) => {
+  var body=req.body
   client.connect(err => {
-    let dbo = client.db("myFirstDatabase");
-    dbo.createCollection("customers", function(err, res) {
+    dbo.collection("logs").insertOne(body, function(err, res) {
       if (err) throw err;
-      console.log("Collection created!");
-      res.send("collection created")
-      db.close();
+      console.log("1 document inserted");
+      dbo.close();
     });
   });
   

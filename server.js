78695483324fcc7dd8a9cf86c945f6631c2ce36d
Riccardo Amadio <riccardo.amadio1@gmail.com>
2021-04-09 12:28:10 +0200

    webhook added

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const port  = process.env.PORT || 80;
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://ciao:ciao@cluster0.zofui.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-let dbo = client.db("myFirstDatabase");
 
 app.post('/webhook', (req, res) => {
   var body=req.body
   client.connect(err => {
+    let dbo = client.db("myFirstDatabase");
     dbo.collection("logs").insertOne(body, function(err, res) {
       if (err) throw err;
       console.log("1 document inserted");

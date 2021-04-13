ef49b8bb238f3809d5e96230f9add0d3db63e1cb
Riccardo Amadio <riccardo.amadio1@gmail.com>
2021-04-13 12:09:23 +0200

    getinfo implemented

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient;
 const hex_to_ascii = require("./utils/hextoascii.js").hex_to_ascii;
-const request= require('request')
+const getInfo = require("./utils/getinfo").getInfo;
 const app = express()
 app.use(express.json());
 const port = process.env.PORT || 3000;
 const uri = "mongodb+srv://ciao:ciao@cluster0.zofui.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const nodo_digitale_eui = "C0EE400001025558"
+const appid="BE7A2562"
 var db;
 
-const headerLoriotAPI = {
-  url: 'https://eu1.loriot.io/1/rest',
-  headers: {
-    'Authorization': 'Bearer vnolYgAAAA1ldTEubG9yaW90LmlvDiJiwQnkwkVoNcNP-ZepCA==',
-    'Cache-Control':'no-cache',
-    'Content-Type': 'application/json'
-  }
-};
 
 client.connect(err => {
   db = client.db("myFirstDatabase");
@@ -30,13 +23,7 @@ app.post('/webhook', (req, res) => {
   if (req.body['EUI'] == nodo_digitale_eui) {
     if (hex_to_ascii(req.body['data']) == 'C') {
       //Accensione
-      request.post(headerLoriotAPI, {json:{cmd:'tx',EUI:"C0EE400001025558",port:2,confirmed:false,data:"0c0303ffff06010c",
-      appid:"BE7A2562"}},function(error, response, body){
-        console.error('error:', error);
-        console.log('statusCode:', response && response.statusCode);
-        console.log('body:', body);
-      })
-
+      getInfo();
     }
     // db.collection("logs").insertOne(req.body, function (err, res) {
     //   if (err) throw err;
---
+++ utils/getinfo.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+exports.getInfo = (nodo_digitale_eui,appid) => {
+    request.post({
+        url: 'https://eu1.loriot.io/1/rest',
+        headers: {
+            'Authorization': 'Bearer vnolYgAAAA1ldTEubG9yaW90LmlvDiJiwQnkwkVoNcNP-ZepCA==',
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'application/json'
+        }
+        , json: {
+            cmd: 'tx',
+            EUI: nodo_digitale_eui,
+            port: 2,
+            confirmed: false,
+            data: "0c0303ffff06010c",
+            appid: appid
+        }
+    }, function (error, response, body) {
+        console.error('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+    })
+}

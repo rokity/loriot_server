b2f6a4511f66bea65b14ba75fb03cb132f43be0c
Riccardo Amadio <riccardo.amadio1@gmail.com>
2021-04-21 10:11:05 +0200

    test db scan sensori

--- server.js
+++ server.js
@@ -25,8 +25,10 @@ client.connect(err => {
 
 app.post('/webhook', (req, res) => {
   const data = req.body['data']
-  if (data == '43') getInfo(req.body['EUI'], appid); //Accensione
-  else if (data.length > 12 && parseInt(data.substring(0, 2)) > -1 && parseInt(data.substring(0, 2)) < 30) scanSensori(db, data, req.body['EUI'])     //Scansione Sensori   
+  if(data!=null){
+    if (data == '43') getInfo(req.body['EUI'], appid); //Accensione
+    else if (data.length > 12 && parseInt(data.substring(0, 2)) > -1 && parseInt(data.substring(0, 2)) < 30) scanSensori(db, data, req.body['EUI'])     //Scansione Sensori   
+  }
   return res.sendStatus(200)
 })
 

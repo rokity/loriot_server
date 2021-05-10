d7fe5e7a7b8f79967dd27d34d99fc032fc6a8106
Riccardo Amadio <riccardo.amadio1@gmail.com>
2021-05-10 14:38:20 +0200

    test limit data package

--- utils/hextoascii.js
+++ utils/hextoascii.js
@@ -28,3 +28,15 @@ exports.HexToFloat32 = (str) => {
         return (float32 * sign);
     } else return 0
 }
+
+exports.hexToSignedInt=(hex) => {
+    if (hex.length % 2 != 0) {
+        hex = "0" + hex;
+    }
+    var num = parseInt(hex, 16);
+    var maxVal = Math.pow(2, hex.length / 2 * 8);
+    if (num > maxVal / 2 - 1) {
+        num = num - maxVal
+    }
+    return num;
+}

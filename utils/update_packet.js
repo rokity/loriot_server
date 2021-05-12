45599ca873d2f08fdbd273983420cd02296eb7e1
Riccardo Amadio <riccardo.amadio1@gmail.com>
2021-05-12 14:43:20 +0200

    fix error update packet

--- utils/update_packet.js
+++ utils/update_packet.js
@@ -5,8 +5,8 @@ exports.updatePacket=(db,data,eui) =>
 {
     console.log("updatePacket")
     const sampling_time= data.substring(2,4) // il sampling_time è formatto in esadecimale come nella tabella 2.1.1 del documento
-    const vcc = parseInt(data.substring(4,6),16)
-    const add_delay= parseInt(data.substring(6,8),16)
+    const vcc = data.substring(4,6)
+    const add_delay= data.substring(6,8)
     db.collection("structures").findOne({"sensors.eui":eui},(err,res)=>{
         if(err) throw err;
         const sensors=res.sensors;
